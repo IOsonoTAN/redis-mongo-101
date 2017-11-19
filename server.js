01bb4314bb858b496413992602bf411ce6aaf7d0
IOsonoTAN <im@tzv.me>
2017-11-19 13:58:39 +0700

    refactored post and get

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const { MongoClient } = require('mongodb')
+const Promise = require('bluebird')
 
 const redisConfig = {
     port: 6379,
     host: '192.168.99.100'
 }
 const redis = require('redis')
-const client = redis.createClient(redisConfig.port, redisConfig.host)
+const client = Promise.promisifyAll(redis.createClient(redisConfig.port, redisConfig.host))
 
 client.on('connect', () => {
     console.log('connect to redis is connected.')
@@ -27,50 +28,37 @@ const port = 3000
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
+const keyGetUsers = 'userlist'
+
 // Get user list
-app.get('/users', (req, res) => {
-    const keyGetUsers = 'userlist'
-
-    client.get(keyGetUsers, (err, result) => {
-        if (err) {
-            console.log('redis get err ->', err)
-        } else if (!result) {
-            // db.users.find()
-            db.collection(collectionUser).find().toArray((err, results) => {
-                if (err) {
-                    console.log('user list err ->', err)
-                }
-
-                // Set user list from Redis
-                client.set(keyGetUsers, JSON.stringify(results), (err, setSuccess) => {
-                    if (err) {
-                        console.log('redis err ->', err)
-                    }
-
-                    console.log('results ->', results)
-
-                    // setSuccess OK
-                    res.send(results)
-                })
-            })
+app.get('/users', async (req, res) => {
+    try {
+        const result = await client.getAsync(keyGetUsers)
+        if (!result) {
+            const dbResults = await db.collection(collectionUser).find().toArray()
+            client.set(keyGetUsers, JSON.stringify(dbResults))
+            res.send(dbResults)
+            return
         }
-
-        // get result from Redis as string
         res.send(JSON.parse(result))
-    })
+    } catch (err) {
+        console.log('err ->', err)
+    }
 })
 
 // Add new user
-app.post('/users', (req, res) => {
-    // db.users.save()
-    db.collection(collectionUser).save(req.body, (err, result) => {
-        if (err) {
-            console.log('save user err ->', err)
-        }
+app.post('/users', async (req, res) => {
+    try {
+        const result = await db.collection(collectionUser).save(req.body)
+        console.log('user has been added.')
 
-        console.log('user has been saved.')
-        res.send('user has been saved.')
-    })
+        await client.delAsync(keyGetUsers)
+        console.log('redis key removed.')
+
+        res.send(req.body)
+    } catch (err) {
+        console.log('err ->', err)
+    }
 })
 
 MongoClient.connect(mongodbUrl, (err, database) => {
